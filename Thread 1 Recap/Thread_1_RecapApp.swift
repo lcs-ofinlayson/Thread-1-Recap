c6897a4ded393b695a0cd46756e7245ee0a72ff5
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-01-10 11:08:10 -0500

    Initial Commit

---
+++ Thread 1 Recap/Thread_1_RecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thread_1_RecapApp.swift
+//  Thread 1 Recap
+//
+//  Created by Oliver Finlayson on 2023-01-10.
+//
+
+import SwiftUI
+
+@main
+struct Thread_1_RecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
